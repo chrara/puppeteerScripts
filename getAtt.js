27ab6e5bc9b518f3b645036f55134ef9c035ff97
Robert Morel <robertchristophermorel@gmail.com>
2019-12-15 13:57:42 +0000

    Dom traversal script for links updated and gettAtt script updated

--- getAtt.js
+++ getAtt.js
@@ -1,5 +1,18 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs');
+
 function run () {
+    /*
+    I want to get a list of urls every hour
+    I want to add these to a file
+    Urls may be duplicates and if so I dont want to add them
+
+    WordPress
+    I want to display the json contents as a list on a page
+    When the user reaches the bottom of the list they should be able to view more
+    I need to paginate the Json file
+
+    */
     return new Promise(async (resolve, reject) => {
         try {
             const browser = await puppeteer.launch({ headless: true });
@@ -12,15 +25,18 @@ function run () {
                     request.abort();
                 }
             });
-            await page.goto("");
+            await page.goto("url");
             let urls = await page.evaluate(() => {
                 let results = [];
                 let items = document.querySelectorAll('#item a');
                 items.forEach((item) => {
+                    if (! item.getAttribute('href').includes("")){
                     results.push({
                         url:  item.getAttribute('href'),
+                        title: item.previousSibling.innerHTML,
                         text: item.innerText,
                     });
+                }
                 });
                 return results;
             })
@@ -31,4 +47,12 @@ function run () {
         }
     })
 }
-run().then(console.log).catch(console.error);
+run().then(
+    function(val) {
+        fs.writeFile(
+            './json/urls.json',
+            JSON.stringify(val, null, 2),
+            (err) => err ? console.error('Data not written!', err) : console.log('Data written!')
+        )
+    }
+).catch(console.error);
